ae5572a32406ccb779b98257a7b999a48726b859
Sofian Smimid <sofian.smimid@etu.unilim.fr>
2025-06-09 22:52:34 +0200

    Initial Commit

---
+++ GoalNow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoalNow
+//
+//  Created by Sofian Smimid on 09/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoalNow/GoalNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalNowApp.swift
+//  GoalNow
+//
+//  Created by Sofian Smimid on 09/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct GoalNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
